6db02697b46718f7f07f9f8fe63027c50891b366
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-03-17 01:44:57 +0530

    new cancel post feature added

--- frontend/instagramCLone/src/components/PostComponents/ShowPosts.jsx
+++ frontend/instagramCLone/src/components/PostComponents/ShowPosts.jsx
@@ -11,6 +11,10 @@ function ShowPosts() {
 
     const {topic,setTopic,Imgurl,setImgurl,createPost} = useContext(UseContext);
 
+    const handleCancelPost = () => {
+      window.location = "/profile";
+    }
+
 
   return (
     <div className='h-screen w-full bg-gray-200 flex items-center justify-center select-none'>
@@ -19,7 +23,10 @@ function ShowPosts() {
               <h1 className=' text-2xl text-white font-bold text-center mb-3'>Post here</h1>
             <input type="text" className='border-none outline-none px-4 py-2 rounded-xl mb-3 text-white bg-slate-600' placeholder='Topic' value={topic} onChange={(e)=>setTopic(e.target.value)}/>
             <input type="text" className='border-none outline-none px-4 py-2 rounded-xl mb-3 text-white bg-slate-600' placeholder='ImgUrl' value={Imgurl} onChange={(e)=>setImgurl(e.target.value)} />
+           <div className='flex gap-3'>
             <button className=' p-3 rounded-xl cursor-pointer bg-blue-500 text-blue-100' onClick={createPost}>Post</button>
+            <button className=' p-3 rounded-xl cursor-pointer bg-red-500 text-red-100' onClick={handleCancelPost}>Cancel</button>
+           </div>
             </div>
         </div>
     </div>
